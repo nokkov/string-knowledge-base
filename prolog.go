203636e7c3a68a6591144b6cde8e74fe63f7ffa4
klinikaa <arkady-chesnokof@yandex.ru>
2023-10-28 18:41:11 +0300

    add ingredients to the knowledge base; TODO: test isPotion() ruke

--- prolog.go
+++ prolog.go
@@ -5,7 +5,34 @@ import "github.com/mndrix/golog"
 func initKnowledgeBase() golog.Machine {
 	kb := golog.NewMachine().Consult(`
 		ingredient(nether_wart).
-		ingredient(glowstone_dust)
+		ingredient(glowstone_dust).
+		ingredient(glistering_melon).
+		ingredient(spider_eyes).
+		ingredient(magma_cream).
+		ingredient(sugar).
+		ingredient(blaze_powder).
+		ingredient(ghast_tears).
+		ingredient(redstone_dust).
+		ingredient(gunpowder).
+		ingredient(golden_carrots).
+		ingredient(rabbits_foot).
+		ingredient(turtle_shell).
+		ingredient(phantom_membrane).
+		ingredient(secret_ingredient).
+
+		%funny ingredients... is it fun huh?!
+		ingredient(water).
+		ingredient(tears).
+		ingredient(mysql).
+		ingredient(javac).
+		ingredient(linux).
+		
+		%3 arguments are passed => this is definitely a potion
+		isPotion(X, Y, Z, Result) :-
+			nonvar(X),
+			nonvar(Y),
+			nonvar(Z),
+			Result = 'Potion!'.
 	`)
 
 	return kb
