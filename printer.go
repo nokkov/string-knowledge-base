e93d728860c2605a53e639ad709e86e668730a33
klinikaa <arkady-chesnokof@yandex.ru>
2023-10-28 20:06:31 +0300

    add PrintRules() & write simple docs

--- printer.go
+++ printer.go
@@ -5,6 +5,7 @@ import (
 	"github.com/fatih/color"
 )
 
+// Info Print author's name, lab's name
 func Info() {
 	fmt.Println("<><><><><><><><><><><>")
 	fmt.Println("<> Chesnokov Arkady <>")
@@ -12,10 +13,22 @@ func Info() {
 	fmt.Println("<><><><><><><><><><><>")
 }
 
+// PrintGreen Print green text to console
 func PrintGreen(message string) {
 	color.Green(message)
 }
 
+// PrintRed Print red text to console
 func PrintRed(message string) {
 	color.Red(message)
+}
+
+// PrintRules Print rules how to make requests to knowledge base
+func PrintRules() {
+	PrintRed("HOW TO\n")
+	PrintRed("ing: ingredients (1..n) => poison exists or not\n")
+	PrintRed("ing: ! => list of all ingredients\n")
+	PrintRed("ing: ? => receipt of random potion\n")
+	PrintRed("ing: * => new potion\n")
+	PrintRed("something else => exit program\n")
 }
